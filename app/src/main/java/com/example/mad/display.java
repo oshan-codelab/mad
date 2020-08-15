a3b7543e9b4e4d1cfcbe82f75b1393dd00f3b0e1
oshan <oshande1999@gmail.com>
2020-08-15 12:29:00 +0530

    mad project

---
+++ app/src/main/java/com/example/mad/display.java
@@ -0,0 +1,17 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display);
+
+       }
+    }
